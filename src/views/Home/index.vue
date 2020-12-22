51212ed63f494ec2eb90312661e3111e72950700
jack <703823834@qq.com>
2020-12-22 19:51:57 +0800

    更新

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>Login</div>
+  <div>
+    <Nav></Nav>
+    <Banner></Banner>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+import Banner from './Banner'
+import Brand from './Brand'
+import Floor from './Floor'
+import Like from './Like'
+import Nav from './Nav'
+import Rank from './Rank'
+import Recommend from './Recommend'
 export default {
-  name: 'Login',
+  name: 'Home',
+  components:{
+    Banner,
+    Brand,
+    Like,
+    Nav,
+    Rank,
+    Recommend,
+    Floor
+  }
 }
 </script>
 
